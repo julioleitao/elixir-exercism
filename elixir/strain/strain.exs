a95ddb2aa7fcb67d22f61ad505e6476002125c2b
Júlio Leitão <contatojuliolmj@gmail.com>
2019-02-16 14:17:04 -0300

    added the solution: strain

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -8,7 +8,12 @@ defmodule Strain do
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def keep(list, fun)
 
-  def keep(enumerable, fun), do: Enum.filter(enumerable, &(fun.(&1)))
+  def keep([], _), do: []
+
+  def keep(enumerable = [head | tail], fun) do
+    apply_at(enumerable, fun, fun.(head), &keep/2)
+  end
+
 
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
@@ -19,5 +24,17 @@ defmodule Strain do
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def discard(list, fun)
 
-  def discard(enumerable, fun), do: Enum.filter(enumerable, &(!fun.(&1)))
+  def discard([], _), do: []
+
+  def discard(enumerable = [head | tail], fun) do
+    apply_at(enumerable, fun, !fun.(head), &discard/2)
+  end
+
+  def apply_at([head | tail], fun_predicate, true, fun_callback) do
+    [head | fun_callback.(tail, fun_predicate)]
+  end
+
+  def apply_at([_ | tail], fun_predicate, false, fun_callback) do
+    fun_callback.(tail, fun_predicate)
+  end
 end
