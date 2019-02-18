1d1ea06a8d113a45f6527f906d13e059c31e1e44
Júlio Leitão <contatojuliolmj@gmail.com>
2019-02-18 19:41:49 -0300

    added the solution: twelve-days

---
+++ elixir/twelve-days/twelve_days.exs
@@ -0,0 +1,66 @@
+defmodule TwelveDays do
+  @days [
+    "first",
+    "second",
+    "third",
+    "fourth",
+    "fifth",
+    "sixth",
+    "seventh",
+    "eighth",
+    "ninth",
+    "tenth",
+    "eleventh",
+    "twelfth"
+  ]
+
+  @gifts %{
+    1 => "a Partridge in a Pear Tree",
+    2 => "two Turtle Doves",
+    3 => "three French Hens",
+    4 => "four Calling Birds",
+    5 => "five Gold Rings",
+    6 => "six Geese-a-Laying",
+    7 => "seven Swans-a-Swimming",
+    8 => "eight Maids-a-Milking",
+    9 => "nine Ladies Dancing",
+    10 => "ten Lords-a-Leaping",
+    11 => "eleven Pipers Piping",
+    12 => "twelve Drummers Drumming"
+  }
+
+  @doc """
+  Given a `number`, return the song's verse for that specific day, including
+  all gifts for previous days in the same line.
+  """
+  @spec verse(number :: integer) :: String.t()
+  def verse(number) do
+    "On the #{Enum.at(@days, number - 1)} day of Christmas my true love gave to me: #{
+      gifts_at(number)
+    }"
+  end
+
+  def gifts_at(n = 1), do: "#{@gifts[n]}."
+
+  def gifts_at(n = 2), do: "#{@gifts[n]}, and #{gifts_at(n - 1)}"
+
+  def gifts_at(n), do: "#{@gifts[n]}, #{gifts_at(n - 1)}"
+
+  @doc """
+  Given a `starting_verse` and an `ending_verse`, return the verses for each
+  included day, one per line.
+  """
+  @spec verses(starting_verse :: integer, ending_verse :: integer) :: String.t()
+  def verses(starting_verse, ending_verse) do
+    Enum.map(starting_verse..ending_verse, fn day ->
+      verse(day)
+    end)
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Sing all 12 verses, in order, one verse per line.
+  """
+  @spec sing() :: String.t()
+  def sing, do: verses(1, 12)
+end
