30f9f1f4fb2db64b54da4c5d474acbeec9f50c45
Júlio Leitão <contatojuliolmj@gmail.com>
2019-02-24 15:07:37 -0300

    added the solution: rna-transcription

---
+++ elixir/rna-transcription/rna_transcription.exs
@@ -0,0 +1,23 @@
+defmodule RNATranscription do
+
+  @dna_to_rna %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A =>?U
+  }
+
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    dna
+    |> Enum.map(&(@dna_to_rna[&1]))
+  end
+end
