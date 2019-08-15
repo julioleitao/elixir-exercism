54742315bbe44e06bb8db4e0d6437638b41cc24b
Júlio Leitão <contatojuliolmj@gmail.com>
2019-08-15 11:31:35 -0300

    added a solution

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,18 @@
+const COLORS_MAPPING = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+};
+
+export const value = (colors) => {
+  const digits = colors.map(color => COLORS_MAPPING[color]);
+
+  return Number(digits.join(''));
+};
